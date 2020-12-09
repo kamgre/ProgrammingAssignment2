09fa04ad6291c338e691f77ff30714290b71e7da
kamgre <kamgrela@gmail.com>
2020-12-09 23:04:56 +0100

    Commented code.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This couple of functions is intended to save some computing time in case of 
+## repeated calls for an inverse of a given matrix. 
 
-## Write a short comment describing this function
+## makeCacheMatrix gets a matrix as an argument and stores it within the 
+## structure of a new object. This object is capable of storing the inverse of 
+## a given matrix to be retrieved later.
+## Intended use is to assign the result to a new objet: 
+## special_matrix <- makeCacheMatrix(ordinary_matrix).
+## 
+## The function assumes that a given matrix IS invertible.
 
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
@@ -14,10 +20,16 @@ makeCacheMatrix <- function(x = matrix()) {
   list(get = get,
        inverse = inverse,
        invert = invert)
+  ## The object being returned is actually a list of functions
 }
 
 
-## Write a short comment describing this function
+## cacheSolve returns an inverse of a matrix, provided that a given matrix was 
+## first stored in a special object created by makeCacheMatrix() before.
+## The inverse is being calculated only once and only at the first run. 
+## Following calls will retrieve the inverse from a value cached in the object's 
+## structure.
+## Intended use it to call for cacheSolve(special_matrix).
 
 cacheSolve <- function(x, ...) {
   inv <- x$inverse()
@@ -25,6 +37,6 @@ cacheSolve <- function(x, ...) {
     message('cached data')
     return(inv) ## Return a matrix that is the inverse of 'x'
   }
-  x$invert()
+  x$invert() ## Calculates only if cached value in unavailable
   x$inverse()        
 }
